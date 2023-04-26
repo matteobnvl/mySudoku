ebb588bf2185917840409e9573c94e6108f95cb4
matteobnvl <matteobonneval19@gmail.com>
2023-04-26 16:28:59 +0200

    add model connexion bdd

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,3 +1,28 @@
 <?php
 
 namespace App\Models;
+
+class User extends Model
+{
+    public static function checkUser($email, $pwd) {
+
+        $db = self::db();
+        $qry = "SELECT * FROM Joueur
+            WHERE email = :email AND mot_de_passe = :password";
+        $stt = $db->prepare($qry);
+        $stt->execute([
+            ':email' => $email,
+            ':password' => md5($pwd)
+        ]);
+        $user = $stt->fetch(\PDO::FETCH_ASSOC);
+
+        if ($stt->rowCount() > 0) {
+
+            foreach ($user as $key => $value) {
+                $_SESSION[$key] = $value;
+            }
+            return true;
+        }
+        return false;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,6 @@
 use App\Route;
 
 Route::get(['/', 'App\Controllers\HomeController@index'])->name('Accueil');
+
+Route::get(['/login', 'App\Controllers\AuthentificationController@login'])->name('Login');
+
